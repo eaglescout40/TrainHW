c26ccc665bfc57b394021422c86751fe73508631
Michael A. Adams, Sr <eaglescout40@gmail.com>
2019-07-23 13:38:03 -0400

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,113 @@
+// Initialize Firebase
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/6.3.1/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#config-web-app -->
+
+ <script>
+//   // Your web app's Firebase configuration
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyCj0Iw2typAYJbMU3zlI5J-MZqbIIGI6bE",
+//     authDomain: "trainhw-81fd4.firebaseapp.com",
+//     databaseURL: "https://trainhw-81fd4.firebaseio.com",
+//     projectId: "trainhw-81fd4",
+//     storageBucket: "trainhw-81fd4.appspot.com",
+//     messagingSenderId: "272239435838",
+//     appId: "1:272239435838:web:eed785329c398e22"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+{
+  "type": "service_account",
+  "project_id": "trainhw-81fd4",
+  "private_key_id": "c80d5514737e9d90ced5f07abd6405b4ee983ae8",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCw+zt8Ze3wT8V9\notmYOytUVGpkornc69OHFqkTzer9o9BNC3kvuyAOkOThASTlRMA6YyYs58Gp+fZ9\n/TYtl2UOronZWbG6pwhB0YOnnvxiY1/QuesmXQ/ln2TPm3gq1i5Mf6ZomDsYoeIk\nZ4e3xCnDXEfYEzNtNxG3TCKUlzHDOfZuUpJ6NHKUleI8AMGFBu+94ayoGDhwyfsE\nRfvp2RiGcJTr0RSkuKM8W6I7li9/BG6Ndv9IiJbv5cIcdKFRxfLpcOiR6asXVLDa\n8Pn+YBvNgICs4vGVePdxazzcGDbU25IGJCQ69Je7FcihBxW13ZycNWHcYyVX+aD5\nXX0pop6JAgMBAAECggEASCqRpZacsP6ePa4/gysxbmgjoBiYqGSDMxUC6b/wP+/h\n2uDAUD3CAx6DLdq9FD32Enjmkq4IEssJ/3LWej2/J/701+uSpNVCcTBBOoMCUA5M\nfjypjdGpz0yxG1S+ad0/MNNmZEUa597u60BKH51+GBrTcuDVegXYFAGYIjYKxRt4\ngMW3DtpftEOan5IBUPt4bY+WBrgCuu9Zl/tNm96bP5IJQW7UTiL0nx3nVaKNvnd2\nsnmb9MFin0dRDem5MZQ95m1KlhZzrspzZLVVqW7mnExBl5z8GZt8KfWrc1AFdY/J\ntG1a4GN413izOaqMCg1RAILoi47AHc4rK19daV18RwKBgQD3JHETU0rMVDm/yeh5\nfozYBM6uJIR/4AvA7Rq7DSe9lLKNpMjfnxvUelCFXfHAdEjke3ijbFHFdO4i5KSN\nyv6E2F7Gd18K5jvMZTIaaspmEz2QlJWoKIoOR1VdgWnONNq8jsEFH2qFZgQS0Nok\nKG7bZq3ApM9jAvYLqAAFzXDbNwKBgQC3Uw5V5tNwSJOD5G9+teGSeu+j0PwIlY0f\nJ4IHdt/G20trgkphbuY0NbnpcTURRaA4udC8mda2WDfJn+gcRQ3avZtcUelAEp0c\n5El6DyCW50NJ4EPwnMDzPORKFxGgamwafW+mETRVBqGTEvis5kGDvkxiYBdY7qOY\nmTO65ha0PwKBgHpa4liM/hUXBdg74Ap2ashv6NsyXR7qDyeeu4MryicDvTp/AKwl\nm6OgQskkb5G2gHgN4PYXpIw7jcJcrFM+0q92Z8WeR8Gfpq9U0m9du4K5t9silM1B\n9MeI7Wxzq6xZYonDqzAcE6KjlRSzrawFEI0y0obzkXEVgDbZH9m/kkYVAoGAP+Xg\nOkrRafKyECfAQP8uV7sn6gZ+wpM19CT2Kpnhl9N570gjmuBntsz/NwHxiEqndP43\nZnWMpFQNZeappajgEl1CWKRoU5/pSVe80KGTL2X+SzKzG5vXmTChNb3is/JEKduM\n3epjPjuod7YGHWXIch4TS7rIiIPtW9QSdtOaGEsCgYEAwf2gJiiGHp82DFnlhRPb\nTIFegBjrFIt6dFdn1I4ajJqTHHfy/AZDGB/DK8pI9k5ZZRlEQo7PnnTQFM+EWRBA\n1D6TnEhJqzzaKLDrwLmFc8jVm6BfOVAzYGdL4+uzQTwOhcWqzKHREHqNrtCJ4Tqo\njD2FUDeQQQrGHx9vp76gb+Q=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-6yai9@trainhw-81fd4.iam.gserviceaccount.com",
+  "client_id": "103325670961548811197",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-6yai9%40trainhw-81fd4.iam.gserviceaccount.com"
+}
+  </script>
+
+
+// Variables
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = 0;
+
+
+// Functions and Click Events
+$("#addTrain").on("click", function() {
+
+  trainName = $('#nameInput').val().trim();
+  destination = $('#destinationInput').val().trim();
+  firstTrainTime = $('#firstTrainInput').val().trim();
+  frequency = $('#frequencyInput').val().trim();
+
+  console.log(trainName);
+  console.log(destination);
+  console.log(firstTrainTime);
+  console.log(frequency);
+
+  database.ref().push({
+    trainName: trainName,
+    destination: destination,
+    firstTrainTime: firstTrainTime,
+    frequency: frequency
+  });
+
+    return false;
+});
+
+
+// Main Code
+database.ref().on("child_added", function(snapshot) {
+  console.log(snapshot.val());
+
+  // update the variable
+  trainName = snapshot.val().trainName;
+  destination = snapshot.val().destination;
+  firstTrainTime = snapshot.val().firstTrainTime;
+  frequency = snapshot.val().frequency;
+
+
+  // moment.js methods
+  var firstTrainMoment = moment(firstTrainTime, 'HH:mm');
+  var nowMoment = moment(); 
+
+  var minutesSinceFirstArrival = nowMoment.diff(firstTrainMoment, 'minutes');
+  var minutesSinceLastArrival = minutesSinceFirstArrival % frequency;
+  var minutesAway = frequency - minutesSinceLastArrival;
+
+  var nextArrival = nowMoment.add(minutesAway, 'minutes');
+  var formatNextArrival = nextArrival.format("HH:mm");
+
+
+  // add table
+  var tr = $('<tr>');
+  var a = $('<td>');
+  var b = $('<td>');
+  var c = $('<td>');
+  var d = $('<td>');
+  var e = $('<td>');
+  a.append(trainName);
+  b.append(destination);
+  c.append(frequency);
+  d.append(formatNextArrival);
+  e.append(minutesAway);
+  tr.append(a).append(b).append(c).append(d).append(e);
+  $('#newTrains').append(tr);
+
+
+  }, function (errorObject) {
+
+  // In case of error this will print the error
+    console.log("The read failed: " + errorObject.code);
+
+});
